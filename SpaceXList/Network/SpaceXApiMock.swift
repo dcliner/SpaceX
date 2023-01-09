11c1e720ab383c2a4aed67413503f617be02da82
derefaacline <cderefaa@gmail.com>
2023-01-09 08:35:48 -0600

    first commit

---
+++ SpaceXList/Network/SpaceXApiMock.swift
@@ -0,0 +1,8 @@
+//
+//  SpaceXApiMock.swift
+//  SpaceXList
+//
+//  Created by Derefaa Cline on 1/8/23.
+//
+
+import Foundation
---
+++ SpaceXList/VM/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  SpaceXList
+//
+//  Created by Derefaa Cline on 1/6/23.
+//
+
+import Foundation
